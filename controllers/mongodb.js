5acd52ad48f50c8e84ffdf19028392cc40b969bf
vigensh s <vigneshrevathy27@gmail.com>
2017-11-29 13:06:14 +0530

    all node request method commited nov-29

--- controllers/mongodb.js
+++ controllers/mongodb.js
@@ -1,8 +1,10 @@
 var bodyParser = require('body-parser');
 var request = require('request-promise');
 var scrape = require('html-metadata');
+var fs = require('fs');
+var url = require('url');
 
-var url = "https://ponyfoo.com";
+var path = "https://ponyfoo.com";
 
 var urlencodedParser = bodyParser.urlencoded({extended:false});
 
@@ -11,21 +13,40 @@ module.exports = function(app){
 
 app.get('/', function(req,res){
 
-     request({
+    request({
         method: 'GET',
-        url: url,
+        url: path,
         json: true,
 
-     }).then((data)=>{
-      res.send(data);
-     });
- });
-
-app.get('/meta', function(req,res){
-  scrape(url).then(function(metadata){
-    console.log(metadata);
-    res.send(metadata);
-  });
+      }).then((data)=>{
+       // console.log(data);
+        fs.writeFileSync('./controllers/delta.json', JSON.stringify(data), 'utf-8');
+            res.json(data);
+          });
 });
+
+app.get('/index/', function(req,res){
+
+  var q = url.parse(req.url, true).query;
+  var path = q.url;
+
+    request({
+          method: 'GET',
+          url: path,
+          json: true,
+          }).then((data)=>{
+              fs.writeFileSync('./controllers/new.json', JSON.stringify(data), 'utf-8');
+              res.json(data);
+          });
+  });
+
+  //scrape method used to get url metadata (hided for future use)
+
+// app.get('/meta', function(req,res){
+//   scrape(url1).then(function(metadata){
+//     console.log(metadata);
+//     res.render('meta', {value:metadata});
+//   });
+// });
 
 } //module.exports function ended
